9dca7a9eac505be78deabbabb736421a344b4d22
ShowyCone <alexrafael2003@gmail.com>
2024-12-07 02:01:39 -0400

    vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test-front-back-db/',
   build: {
-    outDir: 'build', // Asegúrate de que esto esté configurado para generar la carpeta build
+    outDir: 'build',
   },
 })
